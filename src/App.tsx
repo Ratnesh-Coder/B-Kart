55c20ffbff2130f4086e4e46d7166cea544ae781
Ratnesh-Coder <rattneshkumar2024@gmail.com>
2025-11-04 23:07:53 +0530

    Removed duplicate routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,15 @@ import Main from "./components/Main"
 import Details from "./components/details"
 import Sell from "./components/sell";
 
-
-
 const App = () => {
   return (
    <>
-   <Routes>
-   <Route path="/" element={<Main/>}/>
-   <Route path="/details" element={<Details/>}/>
-   <Route path="/" element={<Main />} />
-  <Route path="/details/:id" element={<Details />} />
-  <Route path="/sell" element={<Sell />} />
-   </Routes>
+    <Routes>
+    <Route path="/" element={<Main/>}/>
+    <Route path="/details" element={<Details/>}/>
+    <Route path="/details/:id" element={<Details />} />
+    <Route path="/sell" element={<Sell />} />
+    </Routes>
    </>
   )
 }
